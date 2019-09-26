a532680eae76a90380d538af9537eb4c9803f3a5
Abhilash Tiwari <send.to.abhilash@gmail.com>
2019-09-26 15:47:01 -0400

    Added CORS, working now

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,8 +1,25 @@
+<?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
+<?php header("Access-Control-Allow-Origin: *"); ?>
+
+
+<script type="text/javascript">
+    // jQuery cross domain ajax
+//    $.get("http://test.notetub.com/notetub/index.php?r=user/login").done(function (data) {
+$.get("http://www.1.notetub.com/ajax/ajax.php").done( function (data) {
+    console.log(data['points']);
+//alert("data = " + JSON.stringify(data));
+//alert(JSON.stringify(data['points']));
+$('#points').html(data['points']);
+
+    });
+</script>
+
 <?php $this->pageTitle=Yii::app()->name; ?>
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Congratulations! You now have saved <div id="points">10</div>points.</p>
+
 
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
