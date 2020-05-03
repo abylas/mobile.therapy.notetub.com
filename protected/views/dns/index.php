ef92d281021e8262e697438070e028c25ed19b45
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-05-02 23:12:29 -0400

    backup of how to make this work

--- protected/views/dns/index.php
+++ protected/views/dns/index.php
@@ -15,3 +15,31 @@ $this->menu=array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
 )); ?>
+
+
+<body style = "text-align:center;">
+
+<h1 style = "color:green;" >
+    Voice and/or Text based Presenter on web
+</h1>
+
+<div id= "parent"></div>
+
+<br><br>
+
+<button onclick="insert()">
+    insert
+</button>
+
+<!-- Script to insert div element -->
+<script>
+    function insert() {
+        $("#parent").append('<div id = "newElement">A '
+            + 'Computer Science portal for geeks      <img src="images/blue_arrow.png">                      </div> <br> <br>');
+
+
+    }
+</script>
+</body>
+
+</html>
