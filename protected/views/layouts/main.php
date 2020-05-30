5993ee620f261378db89621361a5ee29c2a94f6f
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-05-30 08:17:39 -0400

    Adding version numbers to icons, maybe that forces them to load, or not anymore?

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,10 +14,10 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1.1">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1.1">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1.1">
+    <link rel="manifest" href="/site.webmanifest?v=1.1">
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
