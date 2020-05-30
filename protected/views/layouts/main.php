b05b28e0edbd3d7a93ce65cbacf1b2fd557de465
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-05-30 05:55:14 -0400

    Adding favicons for Ava

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,6 +14,11 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
