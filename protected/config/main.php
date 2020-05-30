0dbeef5797681eaeef7f600f4080fbab14aef862
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-05-30 07:03:49 -0400

    Updating the name of site again for browser tabs.

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,7 +9,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'AVA.notetub.com',
+	'name'=>'Ava.NoteTub.com',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
