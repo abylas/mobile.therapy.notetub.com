ee8de9ca794a48c630c28b152349bc6cda1dc552
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-06-23 11:11:47 -0400

    Updating create view to include search and managing notes....need to add tags, and audio notes.

--- protected/views/notes/create.php
+++ protected/views/notes/create.php
@@ -7,10 +7,52 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Notes', 'url'=>array('index')),
-	array('label'=>'Manage Notes', 'url'=>array('admin')),
+    array('label'=>'List Notes', 'url'=>array('index')),
+    array('label'=>'Create Notes', 'url'=>array('create')),
+    array('label'=>'Update Notes', 'url'=>array('update', 'id'=>$model->id)),
+    array('label'=>'Delete Notes', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+    array('label'=>'Manage Notes', 'url'=>array('admin')),
 );
+
+
+
+//$this->menu=array(
+//	array('label'=>'List Notes', 'url'=>array('index')),
+//	array('label'=>'Manage Notes', 'url'=>array('admin')),
+//);
 ?>
+
+<h1>Manage Notes</h1>
+
+<p>
+    You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+    or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+</p>
+
+<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<div class="search-form" style="display:none">
+    <?php $this->renderPartial('_search',array(
+        'model'=>$model,
+    )); ?>
+</div><!-- search-form -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+    'id'=>'notes-grid',
+    'dataProvider'=>$model->search(),
+    'filter'=>$model,
+    'columns'=>array(
+        'id',
+        'name',
+        'status',
+        'create_time',
+        'update_time',
+        array(
+            'class'=>'CButtonColumn',
+        ),
+    ),
+)); ?>
+
+
 
 <h1>Create Notes</h1>
 
