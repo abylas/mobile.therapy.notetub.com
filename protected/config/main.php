831d689ffb7826aadb99642aae664ff8a4be162e
send.to.abhilash@gmail.com <Abhiash Tiwari>
2020-06-23 07:36:51 -0700

    updating main.php on the production server

--- protected/config/main.php
+++ protected/config/main.php
@@ -90,7 +90,7 @@ return array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
         ),
-		/*
+		// commen to disable log
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
@@ -106,7 +106,8 @@ return array(
 
 			),
 		),
-		*/
+		
+		//
 	),
 
 	// application-level parameters that can be accessed
