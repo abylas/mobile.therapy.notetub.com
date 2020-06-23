f1098995a1198487f5c270c353f753ed88af43df
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-06-23 09:29:19 -0400

    Adding "Notes" to the header.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -34,6 +34,8 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
+                array('label'=>'Notes', 'url'=>array('/notes/create')),
+
 //				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 //				array('label'=>'Contact', 'url'=>array('/site/contact')),
 //				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
