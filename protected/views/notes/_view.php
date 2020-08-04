c78b20ef29e8ddd9ca102a0b6ff70b76b3790902
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-08-04 18:25:03 -0400

    Adding extra separator on top , Adding breaks in between elements, Adding extra breaks where needed.

--- protected/views/notes/_view.php
+++ protected/views/notes/_view.php
@@ -1,9 +1,11 @@
+———————————————————————————————————————-
 <div class="notes">
     <div class="name">
         <?php
+        echo "<br/>\n";
+
         $this->beginWidget('CMarkdown'
         );
-        echo $data->create_time;
 
         echo nl2br($data->name);
         $this->endWidget();
@@ -13,7 +15,14 @@
         <b>Tags:</b>
         <?php echo implode(', ', $data->tagLinks); ?>
         <br/>
-        <?php echo CHtml::link('Permalink', $data->url); ?> |
+        <?php echo CHtml::link('Permalink', $data->url);
+
+        echo "<br/>\n";
+        echo "<br/>\n";
+
+        echo $data->create_time;
+
+        ?> |
 <!--        --><?php //echo CHtml::link("Comments ({$data->commentCount})",$data->url.'#comments'); ?><!-- |-->
 <!--        Last updated on --><?php //echo date('F j, Y',$data->update_time); ?>
     </div>
